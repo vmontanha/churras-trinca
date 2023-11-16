4055dbc171c3d6dd62426e43f5bae4c40496031b
Vinicius Montanha <viniciusmassis01@hotmail.com>
2023-11-15 21:47:24 -0300

    Unit este with jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Title from './components/Title/Title';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Title />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
